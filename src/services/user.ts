14d1d404f3d55c848b96d095563d565d62b9dc08
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-03-19 08:55:57 +0100

    handle user with localstorage effect

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,9 @@
 import { atom } from "recoil";
 import { User } from "firebase/auth";
+import { localStorageEffect } from "./localStorageEffect";
 
 export const userState = atom({
   key: "User",
   default: null as User | null,
+  effects: [localStorageEffect("user")],
 });
